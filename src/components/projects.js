393cef2562e2b62d81def7f9e8ee83466efa1193
milos-prince <mike.pinder@gmail.com>
2022-08-21 17:18:18 -0400

    updated prototype project

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,7 +3,7 @@ import React from "react"
 const Projects = () => (
 
     <div className="projectsContentSection">
-      <div className="quarterBox bg-jonar"><a href="https://vigorous-curie-0ffff9.netlify.app"><h2>devops dashboard</h2></a><p className="hideOnS">Front end for a server maintenence app. Developed during 4th whale's 2019 hackathon/ shipIT day.</p></div>
+      <div className="quarterBox bg-jonar"><a href="https://vigorous-curie-0ffff9.netlify.app"><h2>devops dashboard</h2></a><p className="hideOnS">Prototype front end for a server maintenence app. Developed during 4th whale's 2019 hackathon/ shipIT day.</p></div>
       <div className="quarterBox bg-evans"><a href="https://www.janeevans.ca"><h2>janeevans.ca</h2></a><p className="hideOnS">Portfolio site built for artist Jane Evans to showcase her work and methods.</p></div>
       <div className="quarterBox bg-ufcw"><a href="https://www.lbplaw.ca"><h2>lbplaw.ca</h2></a><p className="hideOnS">Site for advertising legal services, 2020</p></div>
       <div className="quarterBox bg-paragon"><a href="https://epic-lamport-845b8e.netlify.app/"><h2>hglaw.ca</h2></a><p className="hideOnS">In progress build: a website for the firm Hnatyshyn Gough. Final version waiting on content revisions!</p></div>
